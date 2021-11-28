8d1432be008d1cef4f06a1d2a9e08791103e1815
moFathy57 <mohamed.fathy@bawabtelshrq.com>
2021-11-28 11:13:55 +0200

    REMOVE META DUBLICATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,35 +29,25 @@ export class AppComponent implements OnInit {
           this.blogs = res.data;
           this.blog_datails =  res.data.filter((el :any) => el.id == id)[0];
           this.titleService.setTitle(this.blog_datails.blog_title);
-          this.metaTagService.updateTag([
-            { name: 'keywords', content: this.blog_datails.blog_title },
-            { name: 'robots', content: 'index, follow' },
-            { name: 'author', content: this.blog_datails.blog_title },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'date', content: this.blog_datails.blog_date, scheme: 'YYYY-MM-DD' },
-            { property: 'og:title', content: this.blog_datails.blog_title },
-            { property: 'og:url', content: `https://bawabtelsharq.herokuapp.com/?id=${this.blog_datails.id}` },
-            { property: 'og:image', content: this.blog_datails.blog_image },
-            { property: 'og:description', content: this.blog_datails.blog_title },
-            { charset: 'UTF-8' }
-          ]);
+          this.metaTagService.updateTag({ name: 'keywords', content: this.blog_datails.blog_title }, `content="${this.blog_datails.blog_title}"`);
+          this.metaTagService.updateTag({ name: 'robots', content: 'index, follow' }, `content="index, follow"`);
+          this.metaTagService.updateTag({ name: 'author', content: this.blog_datails.blog_title }, `content="${this.blog_datails.blog_title}"`);
+          this.metaTagService.updateTag({ name: 'date', content: this.blog_datails.blog_date, scheme: 'YYYY-MM-DD' }, `content="${this.blog_datails.blog_date}"`);
+          this.metaTagService.updateTag({ property: 'og:url', content: `https://bawabtelsharq.herokuapp.com/` }, `content="https://bawabtelsharq.herokuapp.com/"`);
+          this.metaTagService.updateTag({ property: 'og:image', content: this.blog_datails.blog_image }, `content="${this.blog_datails.blog_title}"`);
+          this.metaTagService.updateTag({ property: 'og:description', content: this.blog_datails.blog_title }, `content="${this.blog_datails.blog_title}"`);;
         });
       }else{
         this.service.blog().subscribe((res: any) => {
           this.blogs = res.data;
           this.titleService.setTitle(res.data[0].blog_title);
-          this.metaTagService.updateTag([
-            { name: 'keywords', content: res.data[0].blog_title },
-            { name: 'robots', content: 'index, follow' },
-            { name: 'author', content: res.data[0].blog_title },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'date', content: res.data[0].blog_date, scheme: 'YYYY-MM-DD' },
-            { property: 'og:title', content: res.data[0].blog_title },
-            { property: 'og:url', content: `https://bawabtelsharq.herokuapp.com/` },
-            { property: 'og:image', content: res.data[0].blog_image },
-            { property: 'og:description', content: res.data[0].blog_title },
-            { charset: 'UTF-8' }
-          ]);
+          this.metaTagService.updateTag({ name: 'keywords', content: res.data[0].blog_title }, `content="${res.data[0].blog_title}"`);
+          this.metaTagService.updateTag({ name: 'robots', content: 'index, follow' }, `content="index, follow"`);
+          this.metaTagService.updateTag({ name: 'author', content: res.data[0].blog_title }, `content="${res.data[0].blog_title}"`);
+          this.metaTagService.updateTag({ name: 'date', content: res.data[0].blog_date, scheme: 'YYYY-MM-DD' }, `content="${res.data[0].blog_date}"`);
+          this.metaTagService.updateTag({ property: 'og:url', content: `https://bawabtelsharq.herokuapp.com/` }, `content="https://bawabtelsharq.herokuapp.com/"`);
+          this.metaTagService.updateTag({ property: 'og:image', content: res.data[0].blog_image }, `content="${res.data[0].blog_title}"`);
+          this.metaTagService.updateTag({ property: 'og:description', content: res.data[0].blog_title }, `content="${res.data[0].blog_title}"`);
         });
       }
     });
